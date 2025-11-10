1b7336d67dc40d2a72f1346f8a6e70fee3e7ce99
WonChang Oh <owch0731@gmail.com>
2025-11-10 21:25:28 +0900

    코드

---
+++ gitrajabi/src/main/java/com/example/gitrajabi/GitrajabiApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitrajabi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitrajabiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitrajabiApplication.class, args);
+	}
+
+}
---
+++ gitrajabi/src/test/java/com/example/gitrajabi/GitrajabiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitrajabi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitrajabiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
