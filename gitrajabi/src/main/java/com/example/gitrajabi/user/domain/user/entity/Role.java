63ad82fcf9064f4418a67017ececac24db8f7d81
ParkSsol <iamsorry7942@gmail.com>
2025-11-28 16:55:58 +0900

    Delete gitrajabi/src/main/java/com/example/gitrajabi/user directory

--- gitrajabi/src/main/java/com/example/gitrajabi/user/domain/user/entity/Role.java
+++
@@ -1,9 +0,0 @@
-package com.user.firstproject.domain.user.entity;
-
-//package com.user.firstproject.domain.user.entity;
-
-public enum Role {
-    USER,
-    ADMIN
-}
-
--- gitrajabi/src/main/java/com/example/gitrajabi/user/domain/user/service/GithubAuthService.java
+++
@@ -1,4 +0,0 @@
-package com.user.firstproject.domain.user.service;
-
-public class GithubAuthService {
-}
