2671ba7e73fb6783b45e8bb733c09bf7cdb7669c
WonChang Oh <owch0731@gmail.com>
2025-11-29 17:02:21 +0900

    메인 내용 추가

--- gitrajabi/src/main/java/com/example/gitrajabi/GitrajabiApplication.java
+++ gitrajabi/src/main/java/com/example/gitrajabi/GitrajabiApplication.java
@@ -3,8 +3,9 @@ package com.example.gitrajabi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-
+@EnableJpaAuditing
 @SpringBootApplication
 public class GitrajabiApplication {
 
